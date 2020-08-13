c34de4eed8289c3f25f2966cfcdc7aecda6dc410
fengzongbao <fengzongbao@baidu.com>
2020-08-13 11:07:04 +0800

    redesign pcl

---
+++ exercise4-pcl/example/4_1_2_filter.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+import time
+import math
+
+import numpy as np
+import pcl
+import pcl.pcl_visualization
+
+from pcl_helper import get_color_list
+from filtering_helper import do_ransac_plane_segmentation
+from clustering import get_clusters
+
+def main():
+    cloud_filtered = pcl.load('/python-pcl/apollo_scape_pcd/333.pcd')
+    print("cloud points : " + str(cloud_filtered.size))
+
+    # Voxel grid filter
+    vg = cloud_filtered.make_voxel_grid_filter()
+    LEAF_SIZE = 0.09
+    vg.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
+    cloud_filtered = vg.filter()
+    print('point size after voxel_grid_filter: ' + str(cloud_filtered.size))
+
+    # Statistical_outlier_filter
+    stat_filter = cloud_filtered.make_statistical_outlier_filter()
+    stat_filter.set_mean_k(100)
+    stat_filter.set_std_dev_mul_thresh(2.0)
+    cloud_filtered = stat_filter.filter()
+    print('point size after statistical_outlier_filter: ' + str(cloud_filtered.size))
+
+    # ROI Filter
+    points = []
+    for i in range(cloud_filtered.size):
+        # Radius filter
+        r = math.sqrt(cloud_filtered[i][0]*cloud_filtered[i][0] + cloud_filtered[i][1]*cloud_filtered[i][1])
+        # ROI filter Passthrough filter
+        if cloud_filtered[i][2] > -1.55 and cloud_filtered[i][2] < 2.5 and r < 10.0:
+            points.append([cloud_filtered[i][0], cloud_filtered[i][1], cloud_filtered[i][2]])
+
+    cloud_input = pcl.PointCloud()
+    cloud_input.from_list(points)
+
+    # Ransac plane segmentation
+    # plane_cloud, cloud_input = do_ransac_plane_segmentation(cloud_input, max_distance = 0.01)
+
+    # print('object_cloud size %s' % cloud_input.size)
+    # print('plane_cloud size %s' % plane_cloud.size)
+
+    gray_visualizer(cloud_input)
+    # gray_visualizer(plane_cloud)
+
+    print("cloud_input points : " + str(cloud_input.size))
+    # pcl.save(cloud_input, 'cloud_input'+str(time.time())+'.pcd')
+    # exit(0)
+
+def gray_visualizer(cloud):
+    viewer = pcl.pcl_visualization.PCLVisualizering('3D Viewer')
+    pccolor = pcl.pcl_visualization.PointCloudColorHandleringCustom(
+        cloud, 255, 255, 255)
+
+    # viewer
+    viewer.AddPointCloud_ColorHandler(cloud, pccolor)
+
+    v = True
+    while v:
+        v = not(viewer.WasStopped())
+        viewer.SpinOnce()
+
+
+if __name__ == "__main__":
+    main()
