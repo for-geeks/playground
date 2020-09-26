a674dd676709b8d8b57a37216f61731e1f23134c
raosiyue <raosiyue@baidu.com>
2020-09-26 18:15:38 +0800

    add perception

--- exercise4-perception/perception_todo_3.py
+++ exercise4-perception/perception_todo_3.py
@@ -1,15 +1,13 @@
-#!/usr/bin/env python
-import os
-import sys
+# coding:utf-8
 
 import cv2
 import numpy as np
-
-from cyber_py3 import cyber
-
+import os
 from modules.sensors.proto.sensor_image_pb2 import Image
 from modules.planning.proto.planning_pb2 import Trajectory
 from modules.planning.proto.planning_pb2 import Point
+from cyber_py import cyber
+import sys
 
 sys.path.append("../")
 
@@ -23,7 +21,6 @@ M = cv2.getPerspectiveTransform(np.float32(src_corners), np.float32(dst_corners)
 
 car_mid_point = 228
 
-
 def perspective_transform(image, m, img_size=None):
     if img_size is None:
         img_size = (image.shape[1], image.shape[0])
@@ -31,21 +28,21 @@ def perspective_transform(image, m, img_size=None):
     return warped
 
 
-def color_mask(hsv, low, high):
+def color_mask(hsv,low,high):
     # Return mask from HSV
     mask = cv2.inRange(hsv, low, high)
     return mask
 
 
 def apply_yellow_white_mask(img):
-    gradx = abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(20, 255))
+    gradx = abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(80, 180))
     # TODO h
 
     # TODO h END
 
     mask_yellow[(mask_yellow != 0)] = 1
     combined_lsx = np.zeros_like(gradx)
-    combined_lsx[((gradx == 1) | (mask_yellow == 1))] = 1
+    combined_lsx[((gradx == 1) & (mask_yellow == 1))] = 1
     return mask_yellow
 
 
@@ -72,7 +69,7 @@ def get_win_point(leftx, lefty, shape):
                     left_x_0 = leftx[i_y]
                     break
 
-        if left_x_0 == -1:
+        if left_x_0 == -1 :
             tag_y -= 1
             get_polt_tag += 1
             continue
@@ -100,7 +97,7 @@ def get_win_point(leftx, lefty, shape):
     return left_x_re
 
 
-def abs_sobel_thresh(new_image, sobel_kernel=3, orient='x', thresh=(0, 255)):
+def abs_sobel_thresh(image, sobel_kernel=3, orient='x', thresh=(0, 255)):
     # TODO f
 
     # TODO f END
@@ -119,8 +116,8 @@ def abs_sobel_thresh(new_image, sobel_kernel=3, orient='x', thresh=(0, 255)):
 
 
 def translation_view(x, y):
-    x_r = 125.3 - 0.003918 * x - 0.1418 * y
-    y_r = 48.78 - 0.1446 * x - y * 0.008061
+    x_r = 125.3 - 0.003918 * x  - 0.1418 * y
+    y_r = 48.78 - 0.1446 * x  - y * 0.008061
     return x_r, y_r
 
 
@@ -180,10 +177,11 @@ class Exercise(object):
             "/perception/road_mean_point", Trajectory)
 
     def callback(self, data):
+        # TODO
         # print(data.frame_no)
-        #  reshape
+        # TODO reshape
         self.getmeanpoint(data)
-        #  publish, write to channel
+        # TODO publish, write to channel
         if not cyber.is_shutdown():
             self.write_to_channel()
 
@@ -197,7 +195,13 @@ class Exercise(object):
 
         # TODO e END
 
-        wrap_img = perspective_transform(new_image, M, (580, 560))
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (9,9))
+
+        image = cv2.erode(image, kernel)
+
+        image = cv2.blur(image, (5,5))
+
+        wrap_img = perspective_transform(image, M, (580, 560))
 
         yellow_line = apply_yellow_white_mask(wrap_img)
 
